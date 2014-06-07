89fcefb3d0296d86d1effb91381ffdd6801d38a9
rjbosca <ryanbosca@gmail.com>
2014-06-06 21:05:56 -0500

    Created fncs for caching matrix inverse calcs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R contains the functions necessary to perform
+## cached computations of the inverse of a matrix
 
-## Write a short comment describing this function
-
+## makeCacheMatrix  Creates a list of set/get functions
+## for matrix inverse computations
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+
+  ## Define the "set" function
+  set <- function(y){
+          x <<- y
+          m <<- NULL
+  }
+  
+  ## Define the "get" function
+  get <- function() x
+  
+  ## Define the "setinv" function
+  setinv <- function(solve) m <<- solve
+  
+  ## Define the "getinv" function
+  getinv <- function() m
+  
+  ## Finally, create the list of set/get functions
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve calculates the inverse of the matrix
+## object created by makeCacheMatrix or returns the
+## cached value of a previous computation
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## Attempt to get the cached data
+  xinv <- x$getinv()
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+
+  ## Could not get the cached data. Compute inv(x)
+  xinv <- solve(x$get(),...)
+  x$setinv(xinv)
+  xinv
+
 }
